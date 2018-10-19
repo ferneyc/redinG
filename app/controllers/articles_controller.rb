e41b351ff87c99ab4b029d4da751164384233cfb
ferneyc <cferneydario@gmail.com>
2018-10-19 10:06:16 -0500

    guard completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,40 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
+
   def new
   	 @article = Article.new
 
   end
-    def create
-    @article = Article.new(article_params) 
-    @article.save
-    flash[:success] = "Article has been created" 
-    redirect_to articles_path
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article has been created"
+      redirect_to articles_path
+    else
+      flash.now[:danger] = "Article has not been created"
+      render :new
+    end
   end
-private
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+   @article = Article.find(params[:id])
+  end
+
+  private
     def article_params
       params.require(:article).permit(:title, :body) 
+   end
+   protected
+    def resource_not_found
+      message = "The article you are looking for could not be found" 
+      flash[:alert] = message
+      redirect_to root_path
     end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  validates_presence_of :title 
+  validates_presence_of :body
+  default_scope { order(created_at: :desc) }
 end
