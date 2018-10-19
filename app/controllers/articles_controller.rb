49cd118bfdd8277771d7a025fc45b15e513b5765
ferneyc <cferneydario@gmail.com>
2018-10-19 10:57:28 -0500

    completed guard

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,16 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
-  def new
-  	 @article = Article.new
+  def show
+  end
 
+  def new
+    @article = Article.new
   end
 
   def create
@@ -18,19 +23,39 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
   def edit
-    @article = Article.find(params[:id])
   end
 
-  def show
-   @article = Article.find(params[:id])
+  def update
+    if @article.update(article_params)
+      flash[:success] = "Article has been updated"
+      redirect_to @article 
+    else
+      flash.now[:danger] = "Article has not been updated"
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id]) 
+    if @article.destroy
+      flash[:success] = "Article has been deleted."
+      redirect_to articles_path 
+    end
   end
 
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :body) 
-   end
-   protected
+    end
+
+  protected
     def resource_not_found
       message = "The article you are looking for could not be found" 
       flash[:alert] = message
